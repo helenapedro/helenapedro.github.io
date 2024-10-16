a810391d54f7efe46505a9db3cb2e264cb010237
Helena Pedro <mbeua94@gmail.com>
2024-10-16 16:42:37 +0100

    new commit

--- assets/js/header-component.js
+++ assets/js/header-component.js
@@ -4,10 +4,10 @@ class Header extends HTMLElement {
       <header id="header" class="text-center py-5 bg-light">
         <div class="container">
           <div class="row justify-content-center align-items-center">
-            <div class="col-md-4">
-              <img class="img-fluid rounded-circle mb-3" src="images/me1.jpg" alt="">
-            </div>
-            <div class="col-md-8">
+          <div class="col-md-8">
+              <div class="col-md-4">
+                <img class="image avatar" src="images/me1.jpg" alt="">
+              </div>
               <h1 class="display-4">
                 Hi, my name is <a href="https://www.linkedin.com/in/helena-mbeua-pedro/">Helena.</a>
               </h1>
@@ -18,31 +18,39 @@ class Header extends HTMLElement {
               </p>
               <ul class="list-inline mt-4">
                 <li class="list-inline-item">
-                  <a href="https://github.com/helenapedro" target="_blank" class="icon brands fa-github btn btn-dark btn-lg">
-                    <span class="label">Github</span>
+                  <a 
+                    href="https://github.com/helenapedro" 
+                    target="_blank" 
+                    class="icon brands fa-github btn btn-dark btn-lg">
                   </a>
                 </li>
                 <li class="list-inline-item">
-                  <a href="https://www.linkedin.com/in/helena-mbeua-pedro/" target="_blank" class="icon brands fa-linkedin btn btn-primary btn-lg">
-                    <span class="label">LinkedIn</span>
+                  <a 
+                    href="https://www.linkedin.com/in/helena-mbeua-pedro/" 
+                    target="_blank" 
+                    class="icon brands fa-linkedin btn btn-primary btn-lg">
                   </a>
                 </li>
                 <li class="list-inline-item">
-                  <a href="https://pedropublicfiles.s3.us-east-2.amazonaws.com/Resume_HelenaPedro.pdf" target="_blank" rel="noopener noreferrer" class="icon fa-file btn btn-secondary btn-lg">
-                    <span class="label">Resume</span>
+                  <a 
+                    href="https://hmpedropublicfiles.s3.us-east-2.amazonaws.com/Resume_HMPedro.pdf" 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    class="icon fa-file btn btn-secondary btn-lg">
                   </a>
                 </li>
-                <li class="list-inline-item">
-                  <a href="mailto:mbeua94@gmail.com" class="icon fa-envelope btn btn-danger btn-lg">
-                    <span class="label">Email</span>
+                </ul>
+                </div>
+                </div>
+                </div>
+                </header>
+                `;
+                /* <li class="list-inline-item">
+                  <a 
+                    href="mailto:mbeua94@gmail.com" 
+                    class="icon fa-envelope btn btn-danger btn-lg">
                   </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </header>
-    `;
+                </li> */
   }
 }
 customElements.define('header-component', Header);

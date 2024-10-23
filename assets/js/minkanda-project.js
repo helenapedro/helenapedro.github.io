fd702222e644e246482600e6570be4ce68f44d4d
Helena Pedro <mbeua94@gmail.com>
2024-10-23 14:24:35 +0100

    new commit

--- assets/js/minkanda-project.js
+++ assets/js/minkanda-project.js
@@ -9,7 +9,7 @@ class MinkandaProject extends HTMLElement {
           <img src="images/thumbs/hero.webp" class="img-fluid card-img-top" alt="Minkanda Project Thumbnail">
         </a>
         <p class="paragraph">Web app that securely creates, manages, and accesses private and public notes. (Hosted on AWS EC2). Login credentials for testing: (email: test@test.com, password: Test4321)</p>
-         <ul class="actions">
+         <ul class="actions justify-content-center align-items-center">
           <li>
             <a href="https://minkanda.mtcambrosio.com" target="_blank" class="button small">View Project</a>
             <a href="https://github.com/helenapedro/minkanda-frontend.git" target="_blank" class="button small">View Frontend Code</a>

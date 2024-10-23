c0d48b822b858b9c228b7b7cf936cf37bde7b538
Helena Pedro <mbeua94@gmail.com>
2024-10-23 22:11:24 +0100

    new commit

--- assets/js/minkanda-project.js
+++ assets/js/minkanda-project.js
@@ -4,11 +4,12 @@ class MinkandaProject extends HTMLElement {
     <div class="featured card text-center">
       <div class="card-body">
         <h4 class="title">Minkanda: Note Taking</h4>
-        <h6 class="skills mb-2 text-muted">Spring Boot | React | Redux</h6>
+        <h6 class="skills mb-2 text-muted">Spring Boot | Jasypt | EhCache | Hibernate| HikariCP | Logback | Nginx | React | Redux | AWS EC2</h6>
         <a href="https://minkanda.mtcambrosio.com" target="_blank" class="image fit thumb">
           <img src="images/thumbs/hero.webp" class="img-fluid card-img-top" alt="Minkanda Project Thumbnail">
         </a>
-        <p class="paragraph">Web app that securely creates, manages, and accesses private and public notes. (Hosted on AWS EC2). Login credentials for testing: (email: test@test.com, password: Test4321)</p>
+        <p class="paragraph">Web app that securely creates, manages, and accesses private and public notes. 
+        </br> Login credentials for testing: (email: test@test.com, password: Test4321)</p>
         <ul class="actions justify-content-center align-items-center">
           <li>
             <a href="https://minkanda.mtcambrosio.com" target="_blank" class="button small">View Project</a>

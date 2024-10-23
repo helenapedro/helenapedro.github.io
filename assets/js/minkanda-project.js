198ca097b335825bb4000408be77eb2c62c3d460
Helena Pedro <mbeua94@gmail.com>
2024-10-23 14:01:51 +0100

    new commit

--- assets/js/minkanda-project.js
+++ assets/js/minkanda-project.js
@@ -2,13 +2,6 @@ class MinkandaProject extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <div class="featured card text-center">
-      <div class="card-header">
-        <ul class="nav nav-tabs card-header-tabs">
-          <li class="nav-item">
-            <a class="nav-link active" href="https://minkanda.mtcambrosio.com">View Project</a>
-          </li>
-        </ul>
-      </div>
       <div class="card-body">
         <h4 class="title">Minkanda: Note Taking</h4>
         <h6 class="skills mb-2 text-muted">Spring Boot | Spring Security | Spring Data JPA | Hibernate | Jenkins | jasypt | EhCache | Nginx | IP-based Rate Limiting | Load Balancing | HikariCP | Logback | CI/CD | Git | React | Redux | Bootstrap | SCSS</h6>
@@ -16,8 +9,9 @@ class MinkandaProject extends HTMLElement {
           <img src="images/thumbs/hero.webp" class="img-fluid card-img-top" alt="Minkanda Project Thumbnail">
         </a>
         <p class="paragraph">Web app that securely creates, manages, and accesses private and public notes. (Hosted on AWS EC2). Login credentials for testing: (email: test@test.com, password: Test4321)</p>
-        <a href="https://minkanda.mtcambrosio.com" target="_blank" class="btn btn-secondary">View Backend</a>
-        <a href="https://github.com/helenapedro/minkanda-frontend.git" target="_blank" class="btn btn-secondary">View Frontend</a>
+        <a href="https://minkanda.mtcambrosio.com" target="_blank" class="button">View Project</a>
+        <a href="https://github.com/helenapedro/minkanda-frontend.git" target="_blank" class="button m-3">View Frontend Code</a>
+        <a href="https://github.com/helenapedro/notesbackend.git" target="_blank" class="button">View Backend Code</a>
       </div>
     </div>
     `;
